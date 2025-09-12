61c60a381db6d813d2e77c0ad99e2669159f18b9
xavier <XAVIER.HUANG@OOCL.COM>
2025-09-12 11:52:04 +0800

    feat(counter): format code

--- src/components/CounterGroupGenerator.js
+++ src/components/CounterGroupGenerator.js
@@ -7,12 +7,15 @@ export function CounterGroupGenerator(props) {
         }
     }
 
-    function reset(){
+    function reset() {
         props.onUpdateSize(0);
     }
-    return <div>
-        size :
-        <input type={"number"} value={props.counterSize || 0} onChange={updateSize}/>
-        <button onClick={reset}>Reset</button>
-    </div>;
+
+    return <>
+        <div>
+            size :
+            <input type={"number"} value={props.counterSize || 0} onChange={updateSize}/>
+            <button onClick={reset}>Reset</button>
+        </div>
+    </>;
 }
