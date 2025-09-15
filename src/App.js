bd6123d8a87c7e87b27d7e5f6db0c5818a4b580a
xavier <XAVIER.HUANG@OOCL.COM>
2025-09-15 10:43:18 +0800

    feat : router

--- src/App.js
+++ src/App.js
@@ -1,17 +1,44 @@
-import {useReducer} from "react";
 import "./App.css"
 import {todoReducer} from "./reducers/TodoReducer";
 import {TodoContext} from "./contexts/TodoContext";
 import {TodoList} from "./components/TodoList";
+import {createBrowserRouter,Outlet, NavLink, RouterProvider} from "react-router";
+import {useReducer} from "react";
 
 export const initState = [];
+
+const routes = createBrowserRouter([{
+    path: "/",
+    element: <DefaultLayout/>,
+    children: [
+        {
+            path: "/",
+            element: <TodoList />
+        }
+    ]
+}])
+
+function DefaultLayout() {
+    return <div>
+        <header>
+            <nav>
+                <ul>
+                    <li><NavLink to={"/"}>Home</NavLink></li>
+                </ul>
+            </nav>
+            <main>
+                <Outlet/>
+            </main>
+        </header>
+    </div>
+}
 
 function App() {
     const [state, dispatch] = useReducer(todoReducer, initState);
     return (
         <div>
             <TodoContext value={{state, dispatch}}>
-                <TodoList/>
+                <RouterProvider router={routes}/>
             </TodoContext>
         </div>
     );
